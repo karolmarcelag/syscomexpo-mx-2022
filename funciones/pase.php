72a26b7f80ac394ef309798505bdb3e6a716a42b
Karol Gutierrez <karol.gutierrez@syscom.mx>
2022-04-11 10:38:58 -0600

    update

--- funciones/pase.php
+++ funciones/pase.php
@@ -1,83 +1,72 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>SYSCOMExpo 2022</title>
+        <title>Pase SYSCOMExpo 2022</title>
         <link rel="icon" href="https://www.syscom.mx/favico/syscom_favico.png">
         <meta name="viewport" content="width=device-width, initial-scale=1, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <style>
         body
         {
             font-family: system-ui;
-        }
-        .container
-        {
-            width: 491.33858268px;
-            height: 755.98551181px;
+            background: red;
         }
         .cuadroInfo
         {
+            width: 50%;
             transform: rotate(-90deg);
+            margin-left: 20%;
+            margin-top: -23%;
+        }
+        .cuadroDos
+        {
+            margin-top: 30%;
         }
         .nombre
         {
-            font-size: 27px;
-            width: 80%;
+            font-size: 35px;
+            width: 100%;
             text-align: center;
             top: 0;
             left: 0;
             position: absolute;
-            margin-top: 46%;
-            margin-left: -20%;
+            margin-top: 60%;
+            margin-left: -40%;
             font-weight: bold;
         }
         .apellido, .empresa, .cargo
         {
-            font-size: 15px;
-            width: 80%;
+            font-size: 17px;
+            width: 90%;
             text-align: center;
             left: 0;
             top: 0;
             position: absolute;
-            margin-left: -20%;
+            margin-left: -35%;
         }
         .apellido
         {
-            margin-top: 54%;
-            font-size: 21px;
+            margin-top: 76%;
+            font-size: 25px;
             font-weight: bold;
         }
         .empresa
         {
-            margin-top: 67%;
+            margin-top: 99%;
         }
         .cargo
         {
-            margin-top: 94%;
+            margin-top: 146%;
+            margin-left: -53%;
             text-align: center;
             width: 40%;
+            font-size: 14px;
         }
         .imagen
         {
-            margin-top: 83%;
+            margin-top: 140%;
             text-align: center;
-            margin-left: 20%;
-            width: 30%;
-        }
-        .fondo
-        {
-            position: absolute;
-            top: 0;
-            left: 0;
-            z-index: 0;
-            width: 100%;
-        }
-        .cuadoInfo
-        {
-            position: absolute;
-            z-index: 100;
-            width: 100%;
-            left: 0;
-            top: 0;
+            margin-left: 40%;
+            width: 40%;
         }
         </style>
     </head>
@@ -85,17 +74,17 @@
     <!--<body onload="window.print(); window.close();">-->
     <body onload="window.print();">
         <!--<img src="fondo.jpg" class="fondo">-->
-        <div class="container">
-            <div class="cuadroInfo">
-                <div class="nombre"><?php echo $_GET["nombre"] ?></div>
-                <div class="apellido"><?php echo $_GET["apellido"] ?></div>
-                <div class="empresa"><?php echo $_GET["empresa"] ?></div>
-                <div class="cargo"><?php echo $_GET["cargo"] ?></div>
-                <?php
-                echo "<img class='imagen' src='../codigos/".$_GET["correo"].".png'>"
-                ?>
-            </div>
-            <div class="cuadroInfo">
+        <div class="cuadroInfo">
+            <div class="nombre"><?php echo $_GET["nombre"] ?></div>
+            <div class="apellido"><?php echo $_GET["apellido"] ?></div>
+            <div class="empresa"><?php echo $_GET["empresa"] ?></div>
+            <div class="cargo"><?php echo $_GET["cargo"] ?></div>
+            <?php
+            echo "<img class='imagen' src='../codigos/".$_GET["correo"].".png'>"
+            ?>
+        </div>
+        <div class="cuadroInfo">
+            <div class=cuadroDos>
                 <div class="nombre"><?php echo $_GET["nombre"] ?></div>
                 <div class="apellido"><?php echo $_GET["apellido"] ?></div>
                 <div class="empresa"><?php echo $_GET["empresa"] ?></div>
